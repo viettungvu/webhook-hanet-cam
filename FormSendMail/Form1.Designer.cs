1dc22aa6f16de998ceb45a3bd4ccb0c3a244ffd3
Vũ Viết Tùng <viettungtvhd@gmail.com>
2023-11-18 23:14:30 +0700

    Thêm chức năng gửi all

--- FormSendMail/Form1.Designer.cs
+++ FormSendMail/Form1.Designer.cs
@@ -35,9 +35,12 @@
             plFilter = new FlowLayoutPanel();
             groupBox1 = new GroupBox();
             cbxPhongBan = new ComboBox();
+            txtTerminal = new RichTextBox();
             splitter1 = new Splitter();
             tableLayoutPanel1 = new TableLayoutPanel();
             dgvData = new DataGridView();
+            panel2 = new Panel();
+            lblTotal = new Label();
             ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
             splitContainer1.Panel1.SuspendLayout();
             splitContainer1.Panel2.SuspendLayout();
@@ -48,6 +51,7 @@
             groupBox1.SuspendLayout();
             tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dgvData).BeginInit();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
             // splitContainer1
@@ -65,7 +69,7 @@
             // 
             splitContainer1.Panel2.Controls.Add(tableLayoutPanel1);
             splitContainer1.Size = new Size(1128, 580);
-            splitContainer1.SplitterDistance = 311;
+            splitContainer1.SplitterDistance = 428;
             splitContainer1.TabIndex = 0;
             // 
             // tableLayoutPanel2
@@ -80,7 +84,7 @@
             tableLayoutPanel2.RowCount = 2;
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 82.93104F));
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 17.0689659F));
-            tableLayoutPanel2.Size = new Size(307, 580);
+            tableLayoutPanel2.Size = new Size(424, 580);
             tableLayoutPanel2.TabIndex = 1;
             // 
             // panel1
@@ -89,7 +93,7 @@
             panel1.Dock = DockStyle.Fill;
             panel1.Location = new Point(3, 484);
             panel1.Name = "panel1";
-            panel1.Size = new Size(301, 93);
+            panel1.Size = new Size(418, 93);
             panel1.TabIndex = 1;
             // 
             // button1
@@ -106,10 +110,11 @@
             // 
             plFilter.AutoScroll = true;
             plFilter.Controls.Add(groupBox1);
+            plFilter.Controls.Add(txtTerminal);
             plFilter.Dock = DockStyle.Fill;
             plFilter.Location = new Point(3, 3);
             plFilter.Name = "plFilter";
-            plFilter.Size = new Size(301, 475);
+            plFilter.Size = new Size(418, 475);
             plFilter.TabIndex = 2;
             // 
             // groupBox1
@@ -118,7 +123,7 @@
             groupBox1.Dock = DockStyle.Top;
             groupBox1.Location = new Point(3, 3);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(298, 135);
+            groupBox1.Size = new Size(298, 73);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "Phòng/ban";
@@ -130,6 +135,14 @@
             cbxPhongBan.Name = "cbxPhongBan";
             cbxPhongBan.Size = new Size(286, 28);
             cbxPhongBan.TabIndex = 0;
+            // 
+            // txtTerminal
+            // 
+            txtTerminal.Location = new Point(3, 82);
+            txtTerminal.Name = "txtTerminal";
+            txtTerminal.Size = new Size(403, 258);
+            txtTerminal.TabIndex = 1;
+            txtTerminal.Text = "";
             // 
             // splitter1
             // 
@@ -144,13 +157,14 @@
             tableLayoutPanel1.ColumnCount = 1;
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             tableLayoutPanel1.Controls.Add(dgvData, 0, 1);
+            tableLayoutPanel1.Controls.Add(panel2, 0, 0);
             tableLayoutPanel1.Dock = DockStyle.Fill;
             tableLayoutPanel1.Location = new Point(0, 0);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 2;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 9.827586F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 90.17242F));
-            tableLayoutPanel1.Size = new Size(813, 580);
+            tableLayoutPanel1.Size = new Size(696, 580);
             tableLayoutPanel1.TabIndex = 0;
             // 
             // dgvData
@@ -162,8 +176,26 @@
             dgvData.RowHeadersWidth = 51;
             dgvData.RowTemplate.Height = 29;
             dgvData.ShowRowErrors = false;
-            dgvData.Size = new Size(807, 517);
+            dgvData.Size = new Size(690, 517);
             dgvData.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(lblTotal);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(3, 3);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(690, 51);
+            panel2.TabIndex = 1;
+            // 
+            // lblTotal
+            // 
+            lblTotal.AutoSize = true;
+            lblTotal.Location = new Point(3, 31);
+            lblTotal.Name = "lblTotal";
+            lblTotal.Size = new Size(77, 20);
+            lblTotal.TabIndex = 0;
+            lblTotal.Text = "Tổng số: 0";
             // 
             // Form1
             // 
@@ -184,6 +216,8 @@
             groupBox1.ResumeLayout(false);
             tableLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)dgvData).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -199,5 +233,8 @@
         private GroupBox groupBox1;
         private ComboBox cbxPhongBan;
         private DataGridView dgvData;
+        private Panel panel2;
+        private Label lblTotal;
+        private RichTextBox txtTerminal;
     }
 }
--- FormSendMail/Form1.cs
+++ FormSendMail/Form1.cs
@@ -11,6 +11,9 @@ using System.Text.Json;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+using System.ComponentModel;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Xml.Linq;
 
 namespace FormSendMail
 {
@@ -43,15 +46,24 @@ namespace FormSendMail
 
         private void initStrip()
         {
+            menuContext = new ContextMenuStrip();
             ToolStripMenuItem tsGuiBangLuong = new ToolStripMenuItem()
             {
-                Text = "Gủi bảng lương qua email",
+                Text = "Gủi bảng lương cho NV đã chọn",
                 ShortcutKeys = Keys.F10,
-
             };
             tsGuiBangLuong.Click += guiBangLuong;
-            menuContext = new ContextMenuStrip();
-            menuContext.Items.Add(tsGuiBangLuong);
+            //menuContext.Items.Add(tsGuiBangLuong);
+
+
+
+            ToolStripMenuItem tsGuiAllNhanVien = new ToolStripMenuItem()
+            {
+                Text = "Gửi bảng lương cho tất cả NV",
+                ShortcutKeys = Keys.F10,
+            };
+            tsGuiAllNhanVien.Click += guiBangLuongAllNhanVien;
+            menuContext.Items.Add(tsGuiAllNhanVien);
         }
 
         private void openMenuStrip(object? sender, MouseEventArgs arg)
@@ -100,9 +112,9 @@ namespace FormSendMail
             foreach (int userId in selectedUserIds)
             {
                 Employee? em = _context.Employees.Find(userId);
-                em.BusinessEmail = "tungvv@xmedia.vn";
                 if (em != null && !string.IsNullOrWhiteSpace(em.BusinessEmail))
                 {
+                    em.BusinessEmail = "tungvv@xmedia.vn";
                     List<IncomeEmployees> incomes = _context.IncomeEmployess.Where(x => x.UserId == userId).ToList();
                     MailAddress to = new MailAddress(em.BusinessEmail);
                     MailMessage message = new MailMessage(from, to)
@@ -126,6 +138,105 @@ namespace FormSendMail
 
 
             }
+        }
+
+
+
+        BackgroundWorker workerSendMail;
+        private void guiBangLuongAllNhanVien(object? sender, EventArgs arg)
+        {
+            workerSendMail = new BackgroundWorker()
+            {
+                WorkerReportsProgress = true,
+            };
+            workerSendMail.DoWork += sendMailAll;
+            workerSendMail.ProgressChanged += updateSendMailProgress;
+            workerSendMail.RunWorkerAsync();
+        }
+
+        private void updateSendMailProgress(object? sender, ProgressChangedEventArgs e)
+        {
+            string message = Convert.ToString(e.UserState);
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                txtTerminal.AppendText(message + "\n");
+            }
+        }
+
+        private void sendMailAll(object? sender, DoWorkEventArgs e)
+        {
+            int departmentId = -1;
+            if (cbxPhongBan.InvokeRequired)
+            {
+                cbxPhongBan.Invoke(new MethodInvoker(delegate { departmentId = Convert.ToInt32(cbxPhongBan.SelectedValue); }));
+            }
+            IEnumerable<Employee> employees = getEmployees(departmentId).Where(x => !string.IsNullOrEmpty(x.BusinessEmail));
+            if (employees.Any())
+            {
+                workerSendMail.ReportProgress(0, formatMessage("Đang đọc config"));
+                int port = _configuration.GetValue<int>("MailSettings:Port");
+                string host = _configuration.GetValue<string>("MailSettings:Host");
+                string sender_address = _configuration.GetValue<string>("MailSettings:SenderAddress");
+                string sender_name = _configuration.GetValue<string>("MailSettings:SenderName");
+                string password = _configuration.GetValue<string>("MailSettings:SenderSecret");
+
+                workerSendMail.ReportProgress(0, formatMessage("Bắt đầu gửi mail"));
+                SmtpClient mailClient = new SmtpClient(host)
+                {
+                    Port = port,
+                    Credentials = new NetworkCredential(sender_address, password),
+                    EnableSsl = true,
+                };
+
+                mailClient.SendCompleted += OnMailSendCompleted;
+                MailAddress from = new MailAddress(sender_address, sender_name);
+
+                List<Task> tasks = new List<Task>();
+                foreach (var em in employees)
+                {
+                    var emIncomes = _context.IncomeEmployess.Where(x => x.UserId == em.UserId);
+                    if (emIncomes.Any())
+                    {
+                        MailAddress to = new MailAddress(em.BusinessEmail);
+                        MailMessage message = new MailMessage(from, to)
+                        {
+                            Body = JsonSerializer.Serialize(emIncomes),
+                            IsBodyHtml = true,
+                            Subject = string.Format("Bang Luong nhan vien - {0}", em.FullName),
+                        };
+                        Task t = mailClient.SendMailAsync(message);
+                        tasks.Add(t);
+                        workerSendMail.ReportProgress(0, formatMessage(string.Format("Đang gửi cho {0}", em.BusinessEmail)));
+                    }
+                    else
+                    {
+                        workerSendMail.ReportProgress(0, formatMessage(string.Format("Không thấy dữ liệu lương của {0}", em.UserName)));
+                    }
+
+                }
+                Task tAll = Task.WhenAll(tasks.ToArray());
+                try
+                {
+                    tAll.Wait();
+
+                    workerSendMail.ReportProgress(100, formatMessage("Hoàn tất gửi mail"));
+
+                }
+                catch (Exception ex)
+                {
+
+                    workerSendMail.ReportProgress(100, formatMessage(ex.Message));
+                }
+            }
+            else
+            {
+                workerSendMail.ReportProgress(0, formatMessage("Không có dữ liệu"));
+            }
+        }
+
+        private string formatMessage(string message)
+        {
+            return string.Format("[{0}]: {1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), message);
         }
 
         private void OnMailSendCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
@@ -161,6 +272,7 @@ namespace FormSendMail
                 {
                     dgvData.Rows.Add(em.UserId, em.UserName, em.FullName, em.BusinessEmail, em.PositionName);
                 }
+                lblTotal.Text = string.Format("Tổng số: {0}", employees.Count());
             }
         }
 
